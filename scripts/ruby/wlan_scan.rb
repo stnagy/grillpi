4d5d987f4849cf7af43814701ea9f9908230a175
Stephen Nagy <stnagy@gmail.com>
2016-09-30 18:57:55 +0000

    first commit

---
+++ scripts/ruby/wlan_scan.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+# script to output wireless access point information as json
+# requires iwlist utility
+# works on rpi 3 as of Sept. 30, 2016
+
+
+# optionally input wireless interface adapter
+def main(wlan = "wlan0")
+  wifi_output = `sudo iwlist #{wlan} scan`
+  
+  wifi_output_array = wifi_output.split(/Cell \n\n/)
+  return wifi_output_array
+end
+
+# by default, puts the return value from main()
+puts main()
