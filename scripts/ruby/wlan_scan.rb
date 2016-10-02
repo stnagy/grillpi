cabf2319456cc5b9eb208d56286fbc81da26d2c9
Stephen Nagy <stnagy@gmail.com>
2016-10-02 19:58:19 +0000

    updates

--- scripts/ruby/wlan_scan.rb
+++ scripts/ruby/wlan_scan.rb
@@ -4,7 +4,6 @@
 # requires iwlist utility
 # works on rpi 3 as of Sept. 30, 2016
 
-
 # optionally input wireless interface adapter
 def main(wlan = "wlan0")
   # create empty array for results
@@ -17,16 +16,20 @@ def main(wlan = "wlan0")
   wifi_output_array.each_with_index do |r, i|
     next if i == 0 # we don't care about first match, it has no info
     access_info = {} # initialize empty hash for information
+    
+    # match desired information using regex
     access_info[:ssid] = r.match(/ESSID:.+?\n/).to_s[6..-1].strip.gsub('"', '') # remove quotes
     access_info[:quality] = r.match(/Quality=\S+?\s/).to_s[8..-1].strip
     access_info[:signal] = r.match(/Signal level=.+?\n/).to_s[13..-1].strip
     access_info[:mac] = r.match(/Address: \S+?\n/).to_s[9..-1].strip
     access_info[:channel] = r.match(/Channel:\d{1,2}/).to_s[8..-1].strip
     access_info[:frequency] = r.match(/Frequency:\S+? GHz/).to_s[10..-1].strip
+    
+    # add information into array before iterating
     return_array << access_info
   end
   
-  return return_array
+  return return_array 
 end
 
 # by default, puts the return value from main()
