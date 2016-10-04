9c29e07141d8afd8fab7ecb39819a541c3914385
Stephen Nagy <stnagy@gmail.com>
2016-10-04 19:22:05 +0000

    update

--- scripts/ruby/wlan_connect.rb
+++ scripts/ruby/wlan_connect.rb
@@ -14,7 +14,7 @@ def wifi_connect(essid, key)
     # if the wpa_supplicant file already has configuration information for this essid,
     # use sed to remove old essid configuration from file
     # sed tutorial: http://www.grymoire.com/Unix/Sed.html
-    
+    # sudo sed -r '/network/ { N; /Hogwarts Great Hall Wifi/ { N; N; s/network=\{\n\s+ssid="Hogwarts Great Hall Wifi"\n\s+psk.+\n\}// }}' /etc/wpa_supplicant/wpa_supplicant.conf
     `sudo sed -r '/network/ { N; /#{essid}/ { N; N; s/network=.\n\s+ssid=.#{essid}.\n\s+psk.+\n.// }}' /etc/wpa_supplicant/wpa_supplicant.conf`
     # clean up double returns
     `sudo sed -r -i '/^\s*$/ { N; /^\s*$/ { D; D }}' /etc/wpa_supplicant/wpa_supplicant.conf` 
